32e0c46f969c7c6c2e51146d40aa410d91b4e241
Hanumant Pednekar <146935393+hanumant7@users.noreply.github.com>
2024-10-29 21:37:08 +0530

    Oct29

--- w14p8.cpp
+++ w14p8.cpp
@@ -13,12 +13,16 @@ int main()
         else if(n<0)
         throw runtime_error("Error: negative number entered!");
         else if(n%2!=0)
-        throw runtime_error("Error: odd number entered!");
+        throw invalid_argument("Error: odd number entered!");
         else cout<<"It's a even number!";
     }
     catch(const runtime_error& e)
     {
         cout<<e.what()<<endl;
+    }
+    catch(const invalid_argument& f)
+    {
+        cout<<f.what()<<endl;
     }
     return 0;
 }
